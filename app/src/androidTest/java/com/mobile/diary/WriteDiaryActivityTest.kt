78c525ba6a3396d400fc4490edbfbafd123fc9bd
takobrown <joyceleungwy803@gmail.com>
2023-06-25 20:45:36 +0800

    update on Expresso test

--- app/src/androidTest/java/com/mobile/diary/WriteDiaryActivityTest.kt
+++ app/src/androidTest/java/com/mobile/diary/WriteDiaryActivityTest.kt
@@ -6,11 +6,34 @@ import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import android.os.IBinder
+import android.view.WindowManager
+import androidx.test.espresso.Root
+import org.hamcrest.Description
+import org.hamcrest.TypeSafeMatcher
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
+
 @RunWith(AndroidJUnit4::class)
+class ToastMatcher : TypeSafeMatcher<Root?>() {
+
+    override fun describeTo(description: Description) {
+        description.appendText("is toast")
+    }
+
+    override fun matchesSafely(root: Root?): Boolean {
+        val type: Int = root?.windowLayoutParams?.get()?.type ?: return false
+        if (type == WindowManager.LayoutParams.TYPE_TOAST) {
+            val windowToken: IBinder = root.decorView.windowToken
+            val appToken: IBinder = root.decorView.applicationWindowToken
+            return windowToken == appToken
+        }
+        return false
+    }
+
+}
 class WriteDiaryActivityTest {
 
     @Before
