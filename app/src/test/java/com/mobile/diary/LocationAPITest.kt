42b4750dd73170fabcd3d7ba06afcfa99ba67fc3
takobrown <joyceleungwy803@gmail.com>
2023-06-25 22:50:34 +0800

    update on API testing

--- app/src/test/java/com/mobile/diary/LocationAPITest.kt
+++ app/src/test/java/com/mobile/diary/LocationAPITest.kt
@@ -1,14 +1,17 @@
-package com.mobile.diary
-
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.gson.Gson
+import okhttp3.*
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
-import okhttp3.mockwebserver.RecordedRequest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import java.io.File
+import org.junit.runner.RunWith
+import java.io.IOException
+import java.net.HttpURLConnection
 
-class LocationAPITest {
+@RunWith(AndroidJUnit4::class)
+class LocationUnitTest {
 
     private lateinit var mockWebServer: MockWebServer
 
@@ -25,57 +28,71 @@ class LocationAPITest {
 
     @Test
     fun testGetLocation_Success() {
-        // Enqueue a mock response
-        val responseFile = getFileFromResource("location_success_response.json")
-        val mockResponse = MockResponse()
-            .setResponseCode(200)
-            .setBody(getFileContent(responseFile))
+        // Prepare the mock response
+        val mockResponseBody = """
+            {
+                "data": [
+                    {
+                        "label": "Sample Address"
+                    }
+                ]
+            }
+        """.trimIndent()
+        mockWebServer.enqueue(
+            MockResponse()
+                .setResponseCode(HttpURLConnection.HTTP_OK)
+                .setBody(mockResponseBody)
+        )
 
-        mockWebServer.enqueue(mockResponse)
-
-        // Make API request
+        // Set up the test location
         val latitude = 37.7749
         val longitude = -122.4194
-        val url = "http://api.positionstack.com/v1/reverse?access_key=a6d21406ac24c45d11d8279441a24931&query=$latitude,$longitude"
 
-        // Perform the API request using the provided URL
-        // Verify the request
-        val recordedRequest: RecordedRequest = mockWebServer.takeRequest()
-        // Verify the recordedRequest and perform assertions
+        // Invoke the method under test
+        getLocation(latitude, longitude)
 
-        // Verify the response
-
+        // TODO: Add assertions to verify the behavior or state after invoking the method
+        // For example, you can verify if the address is set correctly in the UI component
     }
 
     @Test
     fun testGetLocation_Failure() {
-        // Enqueue a mock response
-        val mockResponse = MockResponse()
-            .setResponseCode(500)
+        // Prepare the mock response for failure scenario
+        mockWebServer.enqueue(MockResponse().setResponseCode(HttpURLConnection.HTTP_NOT_FOUND))
 
-        mockWebServer.enqueue(mockResponse)
-
-        // Make API request
+        // Set up the test location
         val latitude = 37.7749
         val longitude = -122.4194
-        val url = "http://api.positionstack.com/v1/reverse?access_key=a6d21406ac24c45d11d8279441a24931&query=$latitude,$longitude"
 
-        // Perform the API request using the provided URL
-        // Verify the request
-        val recordedRequest: RecordedRequest = mockWebServer.takeRequest()
-        // Verify the recordedRequest and perform assertions
+        // Invoke the method under test
+        getLocation(latitude, longitude)
 
-        // Verify the response
-
+        // TODO: Add assertions to verify the behavior or state after invoking the method
+        // For example, you can verify if the appropriate error handling is done, like showing a toast message
     }
 
-    private fun getFileFromResource(fileName: String): File {
-        val classLoader = javaClass.classLoader
-        return File(classLoader.getResource(fileName)?.file!!)
-    }
+    // Other test cases can be added based on different scenarios, such as network errors, empty responses, etc.
 
-    private fun getFileContent(file: File): String {
-        return file.readText()
-    }
+    private fun getLocation(latitude: Double, longitude: Double) {
+        val okHttpClient = OkHttpClient()
 
+        val url = "http://${mockWebServer.hostName}:${mockWebServer.port}/v1/reverse?access_key=a6d21406ac24c45d11d8279441a24931&query=$latitude,$longitude"
+        val request = Request.Builder().url(url).build()
+
+        val newCall = okHttpClient.newCall(request)
+        newCall.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle failure
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val string = response.body()?.string()
+                    val fromJson = Gson().fromJson(string, AddressBean::class.java)
+                    val get = fromJson?.data?.get(0)
+                    // Update UI component with the address
+                }
+            }
+        })
+    }
 }
