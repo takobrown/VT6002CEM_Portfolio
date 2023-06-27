816f869e575f75555dd7fccfe6e62d8e82f54430
takobrown <94686168+takobrown@users.noreply.github.com>
2023-06-27 17:40:21 +0800

    minor update

--- app/src/main/java/com/mobile/diary/ListDiaryActivity.kt
+++ app/src/main/java/com/mobile/diary/ListDiaryActivity.kt
@@ -2,23 +2,22 @@ package com.mobile.diary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatViewInflater
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.mobile.diary.databinding.ActivityListDiaryBinding
-import com.tencent.mmkv.MMKV
 
 class ListDiaryActivity : AppCompatActivity() {
-    private lateinit var inflate: ActivityListDiaryBinding;
+    private lateinit var inflate: ActivityListDiaryBinding
     private lateinit var diaryList: List<DiaryBean>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         inflate = ActivityListDiaryBinding.inflate(layoutInflater)
         setContentView(inflate.root)
 
-
-        val data = MMKV.defaultMMKV().decodeString("data")
+        val sharedPreferences = getSharedPreferences("MyDiaryPreferences", MODE_PRIVATE)
+        val data = sharedPreferences.getString("data", "")
 
         val listType = object : TypeToken<ArrayList<DiaryBean?>?>() {}.type
         diaryList = if (data.isNullOrEmpty()) {
