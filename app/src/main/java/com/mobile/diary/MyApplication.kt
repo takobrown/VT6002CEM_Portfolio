5434fa87ced3f6b3de94fb321dca010e70669d71
takobrown <94686168+takobrown@users.noreply.github.com>
2023-06-27 17:36:09 +0800

    minor update

--- app/src/main/java/com/mobile/diary/MyApplication.kt
+++ app/src/main/java/com/mobile/diary/MyApplication.kt
@@ -1,12 +1,42 @@
 package com.mobile.diary
 
 import android.app.Application
-import com.tencent.mmkv.MMKV
 
 class MyApplication : Application() {
+    companion object {
+        private const val DIARY_DATA_KEY = "diary_data"
+    }
 
     override fun onCreate() {
         super.onCreate()
-        MMKV.initialize(this)
+
+  
+        val sharedPreferences = applicationContext.getSharedPreferences(
+            "MyDiaryPreferences",
+            MODE_PRIVATE
+        )
+
+  
+        DiaryDataManager.sharedPreferences = sharedPreferences
+    }
+}
+
+object DiaryDataManager {
+    private const val DIARY_DATA_KEY = "diary_data"
+    lateinit var sharedPreferences: SharedPreferences
+
+    fun getDiaryList(): ArrayList<DiaryBean> {
+        val data = sharedPreferences.getString(DIARY_DATA_KEY, "")
+        val listType = object : TypeToken<ArrayList<DiaryBean?>?>() {}.type
+        return if (data.isNullOrEmpty()) {
+            ArrayList()
+        } else {
+            Gson().fromJson(data, listType)
+        }
+    }
+
+    fun saveDiaryList(diaryList: ArrayList<DiaryBean>) {
+        val toJson = Gson().toJson(diaryList)
+        sharedPreferences.edit().putString(DIARY_DATA_KEY, toJson).apply()
     }
 }
